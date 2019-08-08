587610e063624c83fa03418d4a4fb338bf0126c8
Paul1005 <mccarliep@gmail.com>
2019-08-07 23:53:30 -0700

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        try {
+            DecisionTreeDemo.process();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
