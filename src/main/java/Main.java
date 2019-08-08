ce414745dcfb7566f728853c266814e3138f6e59
Paul1005 <mccarliep@gmail.com>
2019-08-08 12:26:43 -0700

    added some stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         try {
             DecisionTreeDemo.process();
+            //KNNDemo.process();
+            //LogisticRegressionDemo.process();
+            //NaiveBayesDemo.process();
+            //RandomForestDemo.process();
+            //SMODemo.process();
         } catch (Exception e) {
             e.printStackTrace();
         }
