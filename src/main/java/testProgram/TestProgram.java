858e2b5814817c91cee35b81a07b7b86128ac571
Paul1005 <mccarliep@gmail.com>
2019-08-12 22:52:20 -0700

    some updates

--- src/main/java/testProgram/TestProgram.java
+++ src/main/java/testProgram/TestProgram.java
@@ -2,7 +2,9 @@ package testProgram;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.meta.AdaBoostM1;
 import weka.classifiers.trees.Id3;
+import weka.classifiers.trees.J48;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
@@ -44,7 +46,7 @@ public class TestProgram {
     public void processData() throws Exception {
         Instances trainingInstances = getData("tennis-train.arff");
 
-        Classifier id3 = new Id3();
+        Id3 id3 = new Id3();
         id3.buildClassifier(trainingInstances);
         System.out.println(id3);
 
@@ -53,6 +55,26 @@ public class TestProgram {
         Instances testingInstances = getData("tennis-test.arff");
 
         evaluation.evaluateModel(id3, testingInstances);
+
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.toMatrixString());
+        System.out.println(evaluation.toClassDetailsString());
+
+        AdaBoostM1 adaBoostM1 = new AdaBoostM1();
+        J48 j48 = new J48();
+        String[] options = new String[3];
+        options[0] = "-R";
+        options[1] = "-N";
+        options[2] = "10";
+        j48.setOptions(options);
+        adaBoostM1.setClassifier(j48);
+        adaBoostM1.setNumIterations(10);
+        adaBoostM1.buildClassifier(trainingInstances);
+        System.out.println(adaBoostM1);
+
+        evaluation = new Evaluation(trainingInstances);
+
+        evaluation.evaluateModel(adaBoostM1, testingInstances);
 
         System.out.println(evaluation.toSummaryString());
         System.out.println(evaluation.toMatrixString());
