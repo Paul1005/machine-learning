9152b140ed71872f962b8f35786636c8564a8d43
Paul1005 <mccarliep@gmail.com>
2019-08-13 23:52:39 -0700

    messing around a bit more

--- src/main/java/testProgram/TestProgram.java
+++ src/main/java/testProgram/TestProgram.java
@@ -2,9 +2,14 @@ package testProgram;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
+import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.meta.AdaBoostM1;
+import weka.classifiers.meta.Bagging;
+import weka.classifiers.meta.Stacking;
+import weka.classifiers.meta.Vote;
 import weka.classifiers.trees.Id3;
 import weka.classifiers.trees.J48;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
@@ -60,14 +65,26 @@ public class TestProgram {
         System.out.println(evaluation.toMatrixString());
         System.out.println(evaluation.toClassDetailsString());
 
-        AdaBoostM1 adaBoostM1 = new AdaBoostM1();
+
         J48 j48 = new J48();
         String[] options = new String[3];
         options[0] = "-R";
         options[1] = "-N";
         options[2] = "10";
         j48.setOptions(options);
-        adaBoostM1.setClassifier(j48);
+        j48.buildClassifier(trainingInstances);
+        System.out.println(j48);
+        
+        evaluation = new Evaluation(trainingInstances);
+
+        evaluation.evaluateModel(j48, testingInstances);
+
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.toMatrixString());
+        System.out.println(evaluation.toClassDetailsString());
+
+        AdaBoostM1 adaBoostM1 = new AdaBoostM1();
+        adaBoostM1.setClassifier(new J48());
         adaBoostM1.setNumIterations(10);
         adaBoostM1.buildClassifier(trainingInstances);
         System.out.println(adaBoostM1);
@@ -75,6 +92,48 @@ public class TestProgram {
         evaluation = new Evaluation(trainingInstances);
 
         evaluation.evaluateModel(adaBoostM1, testingInstances);
+
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.toMatrixString());
+        System.out.println(evaluation.toClassDetailsString());
+
+        Bagging bagging = new Bagging();
+        bagging.setClassifier(j48);
+        bagging.setNumIterations(10);
+        bagging.buildClassifier(trainingInstances);
+        System.out.println(bagging);
+
+        evaluation = new Evaluation(trainingInstances);
+
+        evaluation.evaluateModel(bagging, testingInstances);
+
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.toMatrixString());
+        System.out.println(evaluation.toClassDetailsString());
+
+        Stacking stacking = new Stacking();
+        stacking.setMetaClassifier(new J48());
+        Classifier[] classifiers = {new J48(), new NaiveBayes(), new RandomForest()};
+        stacking.setClassifiers(classifiers);
+        stacking.buildClassifier(trainingInstances);
+        System.out.println(stacking);
+
+        evaluation = new Evaluation(trainingInstances);
+
+        evaluation.evaluateModel(stacking, testingInstances);
+
+        System.out.println(evaluation.toSummaryString());
+        System.out.println(evaluation.toMatrixString());
+        System.out.println(evaluation.toClassDetailsString());
+
+        Vote vote = new Vote();
+        vote.setClassifiers(classifiers);
+        vote.buildClassifier(trainingInstances);
+        System.out.println(vote);
+
+        evaluation = new Evaluation(trainingInstances);
+
+        evaluation.evaluateModel(vote, testingInstances);
 
         System.out.println(evaluation.toSummaryString());
         System.out.println(evaluation.toMatrixString());
