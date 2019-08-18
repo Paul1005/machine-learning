7f56f825d4b4677e412eaa6f59fd68535acb2351
Paul1005 <mccarliep@gmail.com>
2019-08-18 14:54:21 -0700

    setting up system

--- src/main/java/revisionOperator/RevisionOperator.java
+++ src/main/java/revisionOperator/RevisionOperator.java
@@ -2,17 +2,26 @@ package revisionOperator;
 
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.Id3;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 import weka.core.converters.ConverterUtils;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class RevisionOperator {
     private String trainingFile;
     private String testingFile;
+    private String filePath = "src/main/resources/";
+    private ArrayList<Attribute> attributes;
+    private ArrayList<String> attributeNames;
 
     public void run(String csvTrainingFile, String csvTestingFile) throws Exception {
         trainingFile = csvTrainingFile + ".arff";
@@ -29,24 +38,67 @@ public class RevisionOperator {
 
         ArffSaver arffSaver = new ArffSaver();
         arffSaver.setInstances(instances);
-        arffSaver.setFile(new File("src/main/resources/" + csvFile + ".arff"));
+        arffSaver.setFile(new File(filePath + csvFile + ".arff"));
         arffSaver.writeBatch();
     }
 
     private Instances getData(String file) throws Exception {
         ConverterUtils.DataSource trainingSource = new ConverterUtils.DataSource(file);
         Instances instances = trainingSource.getDataSet();
-        instances.setClassIndex(instances.numAttributes()-1);
+        instances.setClassIndex(instances.numAttributes() - 1);
 
         return instances;
     }
 
+    private Instances setUpData() throws Exception {
+        attributes = new ArrayList<>(5);
+        ArrayList<String> outlook = new ArrayList<>();
+        outlook.add("Sunny");
+        outlook.add("Overcast");
+        ArrayList<String> temp = new ArrayList<>();
+        temp.add("Hot");
+        temp.add("Cool");
+        ArrayList<String> humidity = new ArrayList<>();
+        humidity.add("High");
+        humidity.add("Normal");
+        ArrayList<String> wind = new ArrayList<>();
+        wind.add("Strong");
+        wind.add("Weak");
+        ArrayList<String> decision = new ArrayList<>();
+        decision.add("Yes");
+        decision.add("No");
+
+        attributeNames = new ArrayList<>(5);
+        attributeNames.add("Outlook");
+        attributeNames.add("Temp.");
+        attributeNames.add("Humidity");
+        attributeNames.add("Wind");
+        attributeNames.add("Decision");
+
+        attributes.add(new Attribute(attributeNames.get(0), outlook));
+        attributes.add(new Attribute(attributeNames.get(1), temp));
+        attributes.add(new Attribute(attributeNames.get(2), humidity));
+        attributes.add(new Attribute(attributeNames.get(3), wind));
+        attributes.add(new Attribute(attributeNames.get(4), decision));
+
+        Instances instances = new Instances("tennis-test", attributes, 0);
+        instances.setClassIndex(instances.numAttributes() - 1);
+        return instances;
+    }
+
     private void processData() throws Exception {
-        Instances trainingInstances = getData(trainingFile);
+        Instances trainingInstances = setUpData();
+
+        trainingInstances = addInitialSetK("{(Outlook && Temp. && Humidity, 0)}", trainingInstances); // add initial dataSet K
+
+
+        System.out.println(trainingInstances);
+
 
         Id3 id3 = new Id3();
         id3.buildClassifier(trainingInstances);
-        System.out.println(id3);
+
+        trainingInstances.numAttributes();
 
         /*Evaluation evaluation = new Evaluation(trainingInstances);
 
@@ -59,15 +111,37 @@ public class RevisionOperator {
         System.out.println(evaluation.toClassDetailsString());*/
     }
 
-    public void reviseData(){
+    private Instances addInstance(String newLine, Instances instances) throws Exception {
+        double[] newInstance = new double[instances.numAttributes()];
 
+        newInstance[0] = 1;
+        newInstance[1] = 1;
+        newInstance[2] = 1;
+        newInstance[3] = 0;
+        newInstance[4] = 0;
+
+        instances.add(new DenseInstance(1.0, newInstance));
+
+        return instances;
     }
 
-    public void orderStates(){
-
-    }
-
-    public void revisionOperator(){
+    private Instances addInitialSetK(String k, Instances instances) throws Exception {
+        double[] newInstance = new double[instances.numAttributes()];
         
+        instances.add(new DenseInstance(1.0, newInstance));
+
+        return instances;
+    }
+
+    public void reviseData() {
+
+    }
+
+    public void orderStates() {
+
+    }
+
+    public void revisionOperator() {
+
     }
 }
---
+++ src/main/java/revisionOperator/Variable.java
@@ -0,0 +1,9 @@
+package revisionOperator;
+
+import java.util.ArrayList;
+
+public class Variable {
+    private float entropyValue;
+    private String name;
+    private ArrayList<Boolean> values;
+}
