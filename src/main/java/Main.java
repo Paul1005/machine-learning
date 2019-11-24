d043b09419e4ff13330e388eb47c15ae730ae319
Paul1005 <mccarliep@gmail.com>
2019-11-24 15:31:03 -0800

    presentation stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
 import revisionOperator.RevisionOperator;
+import weka.core.Instances;
+import weka.core.converters.ArffSaver;
+import weka.core.converters.CSVLoader;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        File input = new File("src/main/resources/Input.txt"); // grab the text file in the resources folder
+    public static void main(String[] args) throws IOException {
+  /*      File input = new File("src/main/resources/Input.txt"); // grab the text file in the resources folder
 
         TextFileDecrypter textFileDecrypter = new TextFileDecrypter(input); // decrypt the file contents
 
@@ -15,7 +19,7 @@ public class Main {
                 textFileDecrypter.getRevisions(),
                 textFileDecrypter.getAttributeNames(),
                 textFileDecrypter.getAttributes()); // grab all the data and see what we get.
-
+*/
         // The method below tries to do the above but using belief revision operations, it is not complete, but you can uncomment it if you wish to see its output
 
         /*input = new File("src/main/resources/InputOld.txt");
@@ -28,5 +32,20 @@ public class Main {
                 textFileDecrypter.getRevisions(),
                 textFileDecrypter.getAttributeNames(),
                 textFileDecrypter.getAttributes());*/
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.setSource(new File("test.csv"));
+        Instances instances = csvLoader.getDataSet();
+
+        ArffSaver arffSaver = new ArffSaver();
+        arffSaver.setInstances(instances);
+        arffSaver.setFile(new File("src/main/resources/test.arff"));
+        arffSaver.writeBatch();
+
+        try {
+            com.gg.ml.Presentation.processId3();
+            com.gg.ml.Presentation.processLinearRegression();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/Presentation.java
@@ -0,0 +1,96 @@
+package com.gg.ml;
+
+
+
+import java.io.File;
+import java.io.IOException;
+import weka.classifiers.trees.Id3;
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+
+/**
+ * @author Gowtham Girithar Srirangasamy
+ *
+ */
+public class Presentation {
+    /**
+     * This method is to load the data set.
+     * @param fileName
+     * @return
+     * @throws IOException
+     */
+    public static Instances getDataSet(String fileName) throws IOException {
+        /**
+         * we can set the file i.e., loader.setFile("finename") to load the data
+         */
+        /** the arffloader to load the arff file */
+        ArffLoader loader = new ArffLoader();
+        /** load the traing data */
+        loader.setSource(Presentation.class.getResourceAsStream("/" + fileName));
+        /**
+         * we can also set the file like loader3.setFile(new
+         * File("test-confused.arff"));
+         */
+        //loader.setFile(new File(fileName));
+        Instances dataSet = loader.getDataSet();
+        /** set the index based on the data given in the arff files */
+        dataSet.setClassIndex(dataSet.numAttributes()-1);
+        return dataSet;
+    }
+
+    /**
+     * This method is used to process the input and return the statistics.
+     *
+     * @throws Exception
+     */
+    public static void processId3() throws Exception {
+
+        Instances trainingDataSet = getDataSet("decision-train.arff");
+        Instances testingDataSet = getDataSet("decision-test.arff");
+
+        Classifier id3Classifier = new Id3();
+
+        /** */
+        id3Classifier.buildClassifier(trainingDataSet);
+        System.out.println(id3Classifier);
+        /**
+         * train the alogorithm with the training data and evaluate the
+         * algorithm with testing data
+         */
+        Evaluation evalId3 = new Evaluation(trainingDataSet);
+        evalId3.evaluateModel(id3Classifier, testingDataSet);
+        /** Print the algorithm summary */
+        System.out.println(evalId3.toSummaryString());
+        System.out.println(evalId3.toMatrixString());
+        System.out.println(evalId3.toClassDetailsString());
+
+    }
+
+    public static void processLinearRegression() throws Exception {
+
+        Instances trainingDataSet = getDataSet("linear-train.arff");
+        Instances testingDataSet = getDataSet("linear-test.arff");
+        /** Classifier here is Linear Regression */
+        Classifier classifier = new weka.classifiers.functions.LinearRegression();
+        /** */
+        classifier.buildClassifier(trainingDataSet);
+        System.out.println(classifier);
+        /**
+         * train the alogorithm with the training data and evaluate the
+         * algorithm with testing data
+         */
+        Evaluation eval = new Evaluation(trainingDataSet);
+        eval.evaluateModel(classifier, testingDataSet);
+        /** Print the algorithm summary */
+        System.out.println(eval.toSummaryString());
+
+        Instance predicationDataSet = getDataSet("test-confused.arff").lastInstance();
+        double value = classifier.classifyInstance(predicationDataSet);
+        /** Prediction Output */
+        System.out.println(value);
+    }
+}
